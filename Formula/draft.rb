e1af22d307a3c3178cbb37d211dc6bda8da043c5
Matthew Fisher <matt.fisher@microsoft.com>
2017-09-11 11:23:16 -0700

    explicitly add version field
    Homebrew is incorrectly determining the version field as "64" from the binary.

--- Formula/draft.rb
+++ Formula/draft.rb
@@ -1,6 +1,7 @@
 class Draft < Formula
   desc "A tool for developers to create cloud-native applications on Kubernetes."
   homepage "https://github.com/Azure/draft"
+  version "v0.6.0"
   url "https://github.com/Azure/draft/releases/download/v0.6.0/draft-v0.6.0-darwin-amd64.tar.gz"
   sha256 "44f758fde26eaaf40d2cde2edb87cec0f0669dfeaf451a86d8a6788097dba187"
 
