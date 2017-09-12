874a606f33bcf9ca8b43344389e2510d5af33736
Matthew Fisher <matt.fisher@microsoft.com>
2017-09-12 10:07:17 -0700

    add draft-canary formula

---
+++ Formula/draft-canary.rb
@@ -0,0 +1,36 @@
+require 'formula'
+require 'date'
+
+class DraftCanary < Formula
+  desc 'A tool for developers to create cloud-native applications on Kubernetes.'
+  homepage 'https://github.com/Azure/draft'
+  url 'https://azuredraft.blob.core.windows.net/draft/draft-canary-darwin-amd64.tar.gz'
+
+  conflicts_with 'draft-canary', :because => 'multiple version'
+
+  def self.latest_draft_revision
+    @latest_draft_revision ||= begin
+      Date.parse(`curl --silent --HEAD https://azuredraft.blob.core.windows.net/draft/draft-canary-darwin-amd64.tar.gz | grep 'Last-Modified:'`.split(' ', 2).last.strip).to_s
+    end
+  end
+
+  def self.sha256_checksum
+    `curl --silent https://azuredraft.blob.core.windows.net/draft/draft-canary-darwin-amd64.tar.gz.sha256`.split.first
+  end
+
+  sha256 sha256_checksum
+  version "canary-#{latest_draft_revision}"
+
+  def install
+    libexec.install 'draft'
+    (bin/'draft').write_env_script(libexec/'draft', :DRAFT_HOME => etc/'draft')
+  end
+
+  def post_install
+    system bin/'draft', 'init', '--client-only'
+  end
+
+  test do
+    system bin/'draft', 'version', '--client'
+  end
+end
--- Formula/draft.rb
+++ Formula/draft.rb
@@ -1,11 +1,12 @@
 class Draft < Formula
   desc "A tool for developers to create cloud-native applications on Kubernetes."
   homepage "https://github.com/Azure/draft"
-  version "v0.6.0"
-  url "https://github.com/Azure/draft/releases/download/v0.6.0/draft-v0.6.0-darwin-amd64.tar.gz"
+  version "0.6.0"
+  url "https://github.com/Azure/draft/releases/download/v#{version}/draft-v#{version}-darwin-amd64.tar.gz"
   sha256 "44f758fde26eaaf40d2cde2edb87cec0f0669dfeaf451a86d8a6788097dba187"
 
   depends_on "kubernetes-helm" => :recommended
+  conflicts_with 'draft', :because => 'multiple version'
 
   def install
     libexec.install name
