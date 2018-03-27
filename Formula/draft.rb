e3d0797a945119ccb11a1fbd2c73623f82c08594
Matthew Fisher <matt.fisher@microsoft.com>
2018-03-27 16:19:04 -0700

    bump Draft to v0.12.0

--- Formula/draft.rb
+++ Formula/draft.rb
@@ -1,9 +1,9 @@
 class Draft < Formula
   desc "A tool for developers to create cloud-native applications on Kubernetes."
   homepage "https://github.com/Azure/draft"
-  version "0.11.0"
+  version "0.12.0"
   url "https://azuredraft.blob.core.windows.net/draft/draft-v#{version}-darwin-amd64.tar.gz"
-  sha256 "66ebe30fb2fbed704e1b45ee5192f731eff3c84fdd1b44a50745db30079acb8f"
+  sha256 "5caa5cc89d81f193615e3ad55f2c08be59052c3309f7c37d0ed0136d54b82228"
 
   depends_on "kubernetes-helm" => :recommended
 
