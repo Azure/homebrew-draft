f37499b25f5173d920c708fa1433133fa662e872
davidgamero <davidgamero@users.noreply.github.com>
2023-03-15 00:33:17 +0000

    Update draft formula

--- Formula/draft.rb
+++ Formula/draft.rb
@@ -2,9 +2,9 @@
 class Draft < Formula
   desc "Draft is a tool that creates the miminum required files for your Kubernetes deployments."
   homepage "https://github.com/Azure/draft"
-  version "v0.0.30"
-  url "https://github.com/Azure/draft/archive/refs/tags/v0.0.30.tar.gz"
-  sha256 "d33f470284db36a46d525dd3fc2a81d638bec975d0c1833776778a55640f4b31"
+  version "v0.0.31"
+  url "https://github.com/Azure/draft/archive/refs/tags/v0.0.31.tar.gz"
+  sha256 "7285c015b5bc071febada98eabcb52f3015252dbb84ee1ceb782b8710856924a"
   license "MIT"
 
   depends_on "go" => [:build,"1.18"]
