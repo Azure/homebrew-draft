48353d2029458b897d63f198712bafc55edbc620
davidgamero <4248857+davidgamero@users.noreply.github.com>
2024-07-30 17:56:57 +0000

    Update draft formula

--- Formula/draft.rb
+++ Formula/draft.rb
@@ -1,18 +1,17 @@
 class Draft < Formula
   desc "Draft is a tool that creates the miminum required files for your Kubernetes deployments."
   homepage "https://github.com/Azure/draft"
-  version "v0.0.35"
-  url "https://github.com/Azure/draft/archive/refs/tags/v0.0.35.tar.gz"
-  sha256 "9131047d625ea9e0a220af356769ccee9ccd05c6ec5364d56477c8f9698971de"
+  version "v0.0.36"
+  url "https://github.com/Azure/draft/archive/refs/tags/v0.0.36.tar.gz"
+  sha256 "fbd8ff123dcba60dd4630632016d972161f588eaeb7d1959ec5e3bf201ef28b4"
   license "MIT"
 
-  depends_on "go" => [:build,"1.18"]
+  depends_on "go" => [:build,"1.22"]
 
   def install
     ENV.deparallelize
     system "GO11MODULE=on"
     system "make", "go-generate"
-    system "make", "vendor"
     system "go", "build","-v","-ldflags","-X github.com/Azure/draft/cmd.VERSION=%s" % [version],"-o","."
     system "mkdir","#{prefix}/bin"
     system "cp", "draft", "#{prefix}/bin/draft"
